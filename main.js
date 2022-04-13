392ad631c4f38f9b576651a99f08fb9535b8ba66
PhuDao998 <phudaohd98@gmail.com>
2022-04-13 15:49:27 +0700

    hello

---
+++ main.js
@@ -0,0 +1,23 @@
+var fullName = 'Đào Văn Phú';
+var age = 20;
+var languges = new Array(
+    'Javascript',
+    'PHP',
+    'Ruby',
+    'Javascript',
+    'PHP',
+    'Ruby',
+);
+var languges2 = new Array(
+    'Javascript',
+    'PHP',
+    'Ruby',
+    'Javascript',
+    'PHP',
+    'Ruby',
+);
+function cong(a,b) {
+    return a+b;
+};
+var tong = cong(12, 5);
+console.log(tong)
